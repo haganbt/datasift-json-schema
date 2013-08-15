2a57268fd10f6e9ce412b4bdf6bc2cf25dfb7efe
Ben Hagan <benhagan@gmail.com>
2013-08-15 17:44:29 +0100

    Tests - parent item and first child.

--- test/simple-merge-recursive-test.js
+++ test/simple-merge-recursive-test.js
@@ -1,21 +1,40 @@
-// ./node_modules/vows/bin/vows --spec 
+// vows --spec 
 
 var vows 	= require('vows'),
     assert 	= require('assert'),
     merge 	= require('merge-recursive');
 
+// Load test data
+var interaction_parent 									= require('./sample_data/interaction_parent.json');
+var interaction_child_item							= require('./sample_data/interaction_child_item.json');
+var interaction_second_level_child_item = require('./sample_data/interaction_second_level_child_item.json');
+
+
 
 vows.describe('Merge JSON objects recursively').addBatch({
-    'Merging two simple objects': {
+    'Merging a child element': {
          topic: function () { 
 	        return merge.recursive(
-			    { o: { a: 'a', b: 'b' } },
-			    { o: { b: 'c', c: 'd' } }
-			);
+	        	interaction_parent,
+	        	interaction_child_item
+					);
          },
 
-        'contains all parent keys and associated values': function (topic) {
-            assert.deepEqual ({ o: { a: 'a', b: 'c', c: 'd' } }, topic);
+        'contains the "link" key and value': function (topic) {
+        	assert.deepEqual ({"interaction":{"link":"http://twitter.com/foo/1234"}}, topic);
         }
-    }
+    },
+    'Merging a second level child element': {
+         topic: function () { 
+	        return merge.recursive(
+	        	interaction_parent,
+	        	interaction_second_level_child_item
+					);
+         },
+
+        'contains the "author" key and value': function (topic) {
+        	assert.deepEqual ({"interaction":{"link":"http://twitter.com/foo/1234","author":{"username":"syvzwvn"}}}, topic);
+        }
+    }    
+    
 }).export(module); // Export the Suite
