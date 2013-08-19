294b4122fa8a0316a5881a302137ff432dee1bda
Ben Hagan <benhagan@gmail.com>
2013-08-19 12:23:52 +0100

    Tests - merge two child items.

--- test/merge-recursive-test.js
+++ test/merge-recursive-test.js
@@ -14,23 +14,36 @@ vows.describe('Merge JSON objects recursively').addBatch({
 	        	JSON.parse(fs.readFileSync('./test/sample_data/interaction.link.json'))
 					);
          },
-
         'contains the interaction.link key and value': function (topic) {
         	assert.deepEqual ({"interaction":{"link":"http://twitter.com/foo/1234"}}, topic);
         }
    },
+   
     'Merging a second level child element': {
          topic: function () { 
-         	interaction = JSON.parse(fs.readFileSync('./test/sample_data/interaction.json'));
 	        return merge.recursive(
 	        	JSON.parse(fs.readFileSync('./test/sample_data/interaction.json')),
 	        	JSON.parse(fs.readFileSync('./test/sample_data/interaction.author.username.json'))
 					);
          },
-
         'contains the interaction.author.username key and value': function (topic) {
         	assert.deepEqual ({"interaction":{"author":{"username":"syvzwvn"}}}, topic);
         }
-    }  
+    },
+    
+    'Merging two child items': {
+         topic: function () { 
+         	var out = merge.recursive(
+	        	JSON.parse(fs.readFileSync('./test/sample_data/interaction.json')),
+	        	JSON.parse(fs.readFileSync('./test/sample_data/interaction.link.json'))
+					);
+					
+					return merge.recursive(out, JSON.parse(fs.readFileSync('./test/sample_data/interaction.content.json'))
+					);
+         },
+        'contains the interaction.author.link and interaction.author.content keys and values': function (topic) {
+        	assert.deepEqual ({"interaction":{"link":"http://twitter.com/foo/1234","content":"Hello World!"}}, topic);
+        }
+    }   
     
 }).export(module); // Export the Suite
